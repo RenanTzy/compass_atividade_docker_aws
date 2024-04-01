518aa2a9aabc45468be4f19fa7a30c3c46c3286b
RenanTzy <borgesrenan734@gmailcom>
2024-04-01 16:42:08 -0300

    Corrigindo o problema na inicialização do docker

--- user_data.sh
+++ user_data.sh
@@ -2,12 +2,20 @@
 
 sudo yum update -y
 
-sudo yum install docker -y 
 sudo yum install amazon-efs-utils -y
+sudo yum install docker -y 
 
+sudo systemctl start docker.socket
+sudo systemctl enable docker.socket
+
+sudo chmod 666 /var/run/docker.sock
 sudo usermod -aG docker $(whoami)
 
-sudo systemctl start docker
-sudo systemctl enable docker
+sudo curl -L https://github.com/docker/compose/releases/download/v2.26.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 
-sudo mkdir /mnt/efs
+sudo chmod +x /usr/local/bin/docker-compose
+
+mkdir /mnt/efs
+
+sudo systemctl restart --now docker
+
