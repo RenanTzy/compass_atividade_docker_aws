63e75671b961792b21830ad66a5865ac3b8fa0ca
RenanTzy <borgesrenan734@gmailcom>
2024-04-02 22:13:23 -0300

    Corrigindo falha ao iniciar o container, falha na montagem do efs

--- user_data.sh
+++ user_data.sh
@@ -15,7 +15,30 @@ sudo curl -L https://github.com/docker/compose/releases/download/v2.26.0/docker-
 
 sudo chmod +x /usr/local/bin/docker-compose
 
-mkdir /mnt/efs
-
 sudo systemctl restart --now docker
 
+mkdir /mnt/efs
+
+sudo echo "fs-0c31b9d5b408cdb88.efs.us-east-1.amazonaws.com:/ /mnt/efs nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0" >> /etc/fstab
+
+sudo mount -a
+
+cat > /home/ec2-user/docker-compose.yml <<EOF
+version: '3.8'
+services:
+  wordpress:
+    image: wordpress:latest
+    volumes:
+      - /mnt/efs/wordpress:/var/www/html
+    ports:
+      - 80:80
+    environment:
+      WORDPRESS_DB_HOST: wordpress.cju0uqguetsz.us-east-1.rds.amazonaws.com
+      WORDPRESS_DB_USER: admin
+      WORDPRESS_DB_PASSWORD: wordpressadmin
+      WORDPRESS_DB_NAME: wordpress
+      WORDPRESS_TABLE_PREFIX: wp_
+EOF
+
+sudo yum update
+sudo docker-compose -f ~/docker-compose.yml up -d
